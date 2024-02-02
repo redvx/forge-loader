6d2698dc8919c644f31d983bfb1d551370f1e28d
redvx <72520365+redvx@users.noreply.github.com>
2024-02-02 16:26:01 +0100

    Update Client.java

--- src/main/kotlin/dev/redvx/client/Client.java
+++ src/main/kotlin/dev/redvx/client/Client.java
@@ -7,9 +7,9 @@ import java.lang.reflect.Method;
 
 @Mod(modid = Client.MOD_ID, name = Client.MOD_NAME, version = Client.VERSION)
 public class Client {
-    public static final String MOD_ID = "muffin";
-    public static final String MOD_NAME = "Muffin";
-    public static final String VERSION = "0.10";
+    public static final String MOD_ID = "yourclient";
+    public static final String MOD_NAME = "YourClient";
+    public static final String VERSION = "1";
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
